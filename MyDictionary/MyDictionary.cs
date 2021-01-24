79f539e92e364eb182434db009fdd0e7b49c2ecf
canyksl <cany96767@gmail.com>
2021-01-24 13:31:03 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] array; 
+        T[] tempArray;
+
+        public MyDictionary()
+        {
+            array = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            tempArray = array;
+            array = new T[array.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                array[i] = tempArray[i];
+            }
+
+            array[array.Length - 1] = item;
+        }
+
+        public int Length
+        {
+            get { return array.Length; }
+        }
+
+        public T[] Student
+        {
+            get { return array; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> students = new MyDictionary<string>();
+            students.Add("Can");
+            students.Add("Engin");
+
+            Console.WriteLine("Öğrenci Sayısı: "+students.Length);
+
+            Loop(students);
+            
+            students.Add("İrem");
+            Console.WriteLine("***************");
+            Console.WriteLine("Öğrenci Sayısı: " + students.Length);
+            Loop(students);
+            
+        }
+
+        private static void Loop(MyDictionary<string> students)
+        {
+            foreach (var student in students.Student)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
